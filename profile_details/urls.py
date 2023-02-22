0273c84b39724b0b19923be67f4d193909998f4a
Plamen Trendafilov <80783727+PTrendafilov@users.noreply.github.com>
2023-02-23 00:13:50 +0200

    Add files via upload

--- profile_details/urls.py
+++ profile_details/urls.py
@@ -3,4 +3,6 @@ from django.urls import include, path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create_page', views.create_page, name='create_page'),
+    path('<int:id>', views.index, name='index'),
 ]
--- profile_details/views.py
+++ profile_details/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import CV
 from accounts.models import Profile as User
 # Create your views here.
-def index(request):
+def index(request,id):
     content = {}
     return render(request, 'profile-details-page.html')
+def create_page(request):
+    return render(request, 'create-profile-details.html')
