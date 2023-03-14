bd2afeb6338226060af0f6370c0cf305993ce7de
Plamen Trendafilov <80783727+PTrendafilov@users.noreply.github.com>
2023-03-14 22:24:25 +0200

    Chat is started
    Chat is started and the models.py is ready

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from accounts.models import Profile as User
 # Create your models here.
+
+class Room(models.Model):
+    title = models.CharField(max_length=100)
+    users = models.ManyToManyField(User)
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    text = models.TextField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, default=None)
+    date_created = models.DateTimeField(auto_now_add=True)  # new field
+    def __str__(self):
+        return f'{self.text}'
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from accounts.models import *
+from .get_time import *
+from django.http import HttpResponse, HttpResponseForbidden
 # Create your views here.
+
+def index(request):
+    rooms = Room.objects.filter()
+    for room in rooms:
+        all_messages = Message.objects.filter(room=room)
+        room.last_message = all_messages[len(all_messages)-1]
+        room.last_message_time = " ".join(get_time_from_creating(room.last_message.date_created))
+    user = request.user
+    context = {}
+    user_rooms = []
+    for room in rooms:
+        if user in room.users.all():
+            user_rooms.append(room)
+    context = {'rooms':user_rooms}
+    return render(request, 'chat.html', context)
